4e323f7be67e6535c5a23e5e9f715ea8ad40d418
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-03-20 13:43:42 +0100

    Change Leaderboard Activity

--- app/src/main/java/project/mobile/CameraActivity.kt
+++ app/src/main/java/project/mobile/CameraActivity.kt
@@ -1,30 +1,45 @@
 package project.mobile
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import project.mobile.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 //Define Luma Listener for Camera
 typealias LumaListener = (luma: Double) -> Unit
 
+
 class CameraActivity : AppCompatActivity() {
+    //Define the output directory
+    private lateinit var outputDirectory: File
+
     //Start defining camera
     private lateinit var viewBinding: ActivityMainBinding
     private var imageCapture: ImageCapture? = null
@@ -34,8 +49,23 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        //Photo button listener
-        var photobutton = findViewById(R.id.CameraButton) as ImageButton
+        //Preview
+        var photoPreview = findViewById(R.id.viewFinder) as PreviewView
+
+        //Camera button listener
+        var camerabutton = findViewById(R.id.CameraButton) as ImageButton
+        camerabutton.setOnClickListener {
+            takePhoto()
+            if(photoPreview.isVisible) {
+                photoPreview.isVisible = false
+            }
+            else{
+                photoPreview.isVisible = true
+            }
+        }
+
+        outputDirectory = getOutputDirectory()
+
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
 
         // Request camera permissions
@@ -48,17 +78,87 @@ class CameraActivity : AppCompatActivity() {
             )
         }
 
-        // Set up the listeners for take photo button
-        photobutton.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
         cameraExecutor = Executors.newSingleThreadExecutor()
 
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        /*// Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )*/
+        // Get a stable reference of the
+        // modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener,
+        // which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+
+                    // set the saved uri to the image view
+                    findViewById<ImageView>(R.id.iv_capture).visibility = View.VISIBLE
+                    findViewById<ImageView>(R.id.iv_capture).setImageURI(savedUri)
+
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -75,6 +175,8 @@ class CameraActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewFinder.createSurfaceProvider())
                 }
 
+            imageCapture = ImageCapture.Builder().build()
+
             // Select front camera as a default
             val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
 
@@ -83,8 +185,10 @@ class CameraActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
+                //cameraProvider.bindToLifecycle( this, cameraSelector, preview)
+
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -126,5 +230,14 @@ class CameraActivity : AppCompatActivity() {
                 Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    // creates a folder inside internal storage
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
     }
 }
